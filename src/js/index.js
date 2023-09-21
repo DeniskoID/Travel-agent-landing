0acd94984689234f7d6323cd0b86cd8ba5fc2584
Denis <80954428+DeniskoID@users.noreply.github.com>
2023-09-21 23:07:08 +0300

    Creates a hero-section

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,5 @@
 import mobileNav from './modules/mobile-nav.js';
 mobileNav();
+
+import animateSvg from './utilits/animate-svg.js';
+animateSvg();
---
+++ src/js/utilits/animate-svg.js
@@ -0,0 +1,8 @@
+function animateSvg() {
+  // Mobile nav button
+  const svgElem = document.querySelector('.animated-svg');
+
+  svgElem.classList.toggle('active');
+}
+
+export default animateSvg;
